a44b4affe4c62c0c7459e858d2d7fc24971dfeba
Playground <playground@CKO-3135.local>
2020-08-31 12:16:49 +0100

    first commit

---
+++ src/chatbot.h
@@ -0,0 +1,72 @@
+#ifndef CHATBOT_H_
+#define CHATBOT_H_
+
+#include <iostream>
+#include <wx/bitmap.h>
+#include <string>
+
+class GraphNode; // forward declaration
+class ChatLogic; // forward declaration
+
+class ChatBot
+{
+private:
+    // data handles (owned)
+    wxBitmap *_image; // avatar image
+
+    // data handles (not owned)
+    GraphNode *_currentNode;
+    GraphNode *_rootNode;
+    ChatLogic *_chatLogic;
+
+    // proprietary functions
+    int ComputeLevenshteinDistance(std::string s1, std::string s2);
+
+public:
+    // constructors / destructors
+    ChatBot();                     // constructor WITHOUT memory allocation
+    ChatBot(std::string filename); // constructor WITH memory allocation
+    ~ChatBot();
+    
+    //// STUDENT CODE
+    ////
+    ChatBot(ChatBot &source);
+
+    ChatBot &operator = (ChatBot &source) {
+        if (this == &source)
+            return *this;
+
+        _image = source._image;
+        source._image = nullptr;
+        std::cout << "ChatBot Copy Assignment Operator" << std::endl;
+
+        return *this;
+    };
+
+    ChatBot(ChatBot &&source);
+
+    ChatBot &operator= (ChatBot &&source) {
+        if (this == &source) 
+            return *this;
+
+        delete _image;
+
+        _image = source._image;
+        source._image = nullptr;
+
+        return *this;
+    }
+    ////
+    //// EOF STUDENT CODE
+
+    // getters / setters
+    void SetCurrentNode(GraphNode *node);
+    void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
+    void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
+    wxBitmap *GetImageHandle() { return _image; }
+
+    // communication
+    void ReceiveMessageFromUser(std::string message);
+};
+
+#endif /* CHATBOT_H_ */
